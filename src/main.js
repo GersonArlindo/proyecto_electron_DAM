ca0999c248c271a2aa11c3fe321c768df2a769ef
GersonArlindo <gerson50039@gmail.com>
2022-10-03 19:28:52 -0600

    agregando mostrar productos

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { BrowserWindow } = require('electron') //Nos permite pintar una ventana por pantalla
+const { BrowserWindow, Notification } = require('electron') //Nos permite pintar una ventana por pantalla
 const { getConnection } = require('./database') //Importamos la funcion de coneccion
 
 
@@ -8,10 +8,33 @@ async function createProduct(product) { //Funcion que nos servira mas adelante p
         product.price = parseFloat(product.price) //Conversion del precio del producto
         const result = await conn.query('INSERT INTO product SET ?', product)
         console.log(result)
+
+        //27. Retornando el producto para mostrarlo
+        product.id = result.insertId;
+        return product;
+
     } catch (error) {
         console.log(error)
     }
 }
+
+//30. Creando la funcion getProducts
+async function getProducts() {
+    const conn = await getConnection();
+    const results = await conn.query('SELECT * FROM product ORDER BY id DESC')
+    console.log(results)
+    return results;
+}
+
+//43. deleteProduct
+async function deleteProduct(id) {
+    const conn = await getConnection();
+    const result = await conn.query('DELETE FROM product WHERE id = ?', id);
+    console.log(result)
+    return result;
+}
+
+
 //CREANDO LA VENTANA
 let window
 function createWindow() { //Cuando se llame creara una nueva instancia de BrowserWindow
@@ -28,5 +51,7 @@ function createWindow() { //Cuando se llame creara una nueva instancia de Browse
 
 module.exports = { //Vamos a exportar la funcion para que sea utilizada en otros lados 
     createWindow,
-    createProduct
+    createProduct,
+    getProducts,
+    deleteProduct
 }
--- src/ui/app.js
+++ src/ui/app.js
@@ -5,13 +5,82 @@ const main = remote.require('./main') //Desde donde va a requerir el archivo
 const productName = document.getElementById('name')
 const productPrice = document.getElementById('price')
 const productDescripcion = document.getElementById('descripcion')
+const productsList = document.getElementById('products')
 
-productForm.addEventListener('submit', (e) =>{
+//36. Creamos una lista vacia
+let products = []
+
+
+productForm.addEventListener('submit', async (e) =>{
     e.preventDefault(); 
     const newProduct = { //Objeto para enviar los valores de los input
         name: productName.value,
         price: productPrice.value,
         descripcion: productDescripcion.value
     }
-    main.createProduct(newProduct);
+    //28. Agregando el newProduct en una variable y convirtiendolo en async await linea 9 y 17
+    const result = await main.createProduct(newProduct);
+    console.log(result);
+
+    //42. Reseteo del formulario
+    productForm.reset();
+    productName.focus();
+
+    //41. Llamando a getProducts();
+    getProducts();
 })
+
+//29. creamos otra funcion getProducts para obtener el producto
+const getProducts = async () => {
+    //const results = await main.getProducts();
+    //console.log(results);
+
+    //37. Lista vacia agregando los productos
+    products = await main.getProducts();
+    renderProducts(products); //pasando los productos a la funcion renderProductos
+}
+
+
+//44. Creando funcion para deleteProduct
+async function deleteProduct(id){
+   const response = confirm('Desea eliminar este producto?')
+   if(response){
+    await main.deleteProduct(id);
+    await getProducts();
+   }else{
+    return;
+   }
+   
+}
+
+//34. Creamos la funcion renderProducts la cual recibira la lista de productos
+function renderProducts(products){
+    productsList.innerHTML = ''; //limpiar cada que se llame para evitar duplicidad de datos
+    products.forEach(product => { //Añadiendo los elementos a la lista para pintarlos
+        //Lo colocamos entre comillas oblicuas para poner varias lineas
+        productsList.innerHTML += ` 
+            <div class="card card-body my-2 animated fadeInLeft">
+            <h4>${product.name}</h4>
+            <p>${product.descripcion}</p>
+            <h3>${product.price}</h3>
+            <p>
+                <button class="btn btn-danger" onclick="deleteProduct('${product.id}')">
+                    Eliminar
+                </button>
+                <button class="btn btn-secondary">
+                    Editar
+                </button>
+            </p>
+            </div>
+        `;
+    });
+}
+
+//getProducts();
+
+//35. agregando la funcion init()
+async function init(){
+    await getProducts();
+}
+
+init();
